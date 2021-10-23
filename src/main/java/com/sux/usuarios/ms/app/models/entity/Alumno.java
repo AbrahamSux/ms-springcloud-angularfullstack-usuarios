aacd9e9ee75280561afb3b30fde1847b84a2707b
Abraham Juárez <abraham-aj@hotmail.com>
2021-10-23 18:27:54 -0500

    Sección 02: Backend - MS Usuarios
    Descripción y/o Comentarios:  Se agrega la entidad de Alumno.

---
+++ src/main/java/com/sux/usuarios/ms/app/models/entity/Alumno.java
@@ -0,0 +1,109 @@
+/*
+ * Proyecto:        MS - USUARIOS
+ * A.J. SUX  – Todos los derechos reservados. Para uso exclusivo de Abraham Juárez de la Cruz.
+ */
+
+package com.sux.usuarios.ms.app.models.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @description Entidad que representa a un Alumno dentro del sistema.
+ *
+ * @author Abraham Juárez de la Cruz - ajuarezdelacruz93@gmail.com
+ * @creationDate 23/10/2021 18:27:00 hrs.
+ */
+@Entity
+@Table(name = "alumnos")
+public class Alumno implements Serializable {
+
+    private static final long serialVersionUID = -176473860299958216L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @Column(name = "apellido_paterno")
+    private String apellidoPaterno;
+
+    @Column(name = "apellido_materno")
+    private String apellidoMaterno;
+
+    private String email;
+
+    @Column(name = "create_at")
+    private Date createAt;
+
+
+
+    // METODOS
+
+    @PrePersist
+    public void prePersist() {
+        this.createAt = new Date();
+    }
+
+
+
+    // GETTERS Y SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+}
