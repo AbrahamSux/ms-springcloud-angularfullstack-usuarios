73b5ead1bb4719270d2e3896e1260027f1ca3e42
AbrahamJuarez <ajuarezdelacruz93@gmail.com>
2021-10-23 17:03:25 -0500

    Inicializando el proyecto. MS Usuarios.

---
+++ src/main/java/com/sux/usuarios/ms/app/MsSpringcloudAngularfullstackUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.sux.usuarios.ms.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringcloudAngularfullstackUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringcloudAngularfullstackUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sux/usuarios/ms/app/MsSpringcloudAngularfullstackUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sux.usuarios.ms.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringcloudAngularfullstackUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
